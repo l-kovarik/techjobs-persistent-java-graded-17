1e5f98b6793b381e77070c878400198a642a477d
lorikovarik80 <kovarik.lori@gmail.com>
2023-11-11 00:13:26 -0600

    Task 3 complete

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import jakarta.validation.Valid;
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,6 +22,15 @@ import java.util.Optional;
 @Controller
 public class HomeController {
 
+    @Autowired
+    EmployerRepository employerRepository;
+
+    @Autowired
+    JobRepository jobRepository;
+
+    @Autowired
+    SkillRepository skillRepository;
+
     @RequestMapping("/")
     public String index(Model model) {
 
@@ -28,20 +42,29 @@ public class HomeController {
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
 	model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
 
     @PostMapping("add")
-    public String processAddJobForm(@ModelAttribute @Valid Job newJob,
+    public String processAddJobForm(@ModelAttribute @Valid Job newJob, @RequestParam("employerId")
                                        Errors errors, Model model, @RequestParam int employerId) {
 
         if (errors.hasErrors()) {
 	    model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
             return "add";
         }
 
-        return "redirect:";
+        Optional<Employer> optEmployer = employerRepository.findById(employerId);
+        if (optEmployer.isPresent()) {
+            Employer employer = optEmployer.get();
+            newJob.setEmployer(employer);
+        }
+
+        jobRepository.save(newJob);
+        return "redirect:./";
     }
 
     @GetMapping("view/{jobId}")
